8e17950659680cc9ad297dfe68aa6d646a83d13b
김창규 <kimc980106@naver.com>
2023-01-04 23:21:49 +0900

    Initial Commit

---
+++ src/test/java/com/prgrms/devcourse/springbooturlshortener/SpringbootUrlShortenerApplicationTests.java
@@ -0,0 +1,9 @@
+package com.prgrms.devcourse.springbooturlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUrlShortenerApplicationTests {
+
+}
